124e1b10394767a53c64bcbcd93721c996fb8bab
cskrub33 <cskrub33@gmail.com>
2024-10-31 16:32:13 +0900

    change, some wrong code

--- chat.js
+++ chat.js
@@ -1,17 +1,19 @@
-varsocket=io();
-varmessages=document.getElementById('messages');
-varform=document.getElementById('form');
-varinput=document.getElementById('input');
-form.addEventListener('submit',function(e){
-e.preventDefault();
-if(input.value){
-socket.emit('chatmessage',input.value);
-input.value='';
-}
+var socket = io();
+var messages = document.getElementById("messages");
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chatmessage", input.value);
+    input.value = "";
+  }
 });
-socket.on('chatmessage',function(msg){
-varitem=document.createElement('li');
-item.textContent=msg;
-messages.appendChild(item);
-window.scrollTo(0,document.body.scrollHeight);
+
+socket.on("chatmessage", function (msg) {
+  var item = document.createElement("li");
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 });
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-constapp=require('express')();
-consthttp=require('http').Server(app);
-constio=require('socket.io')(http);
-app.get('/',function(req,res){
-res.sendFile(__dirname+'/index.html');
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
-app.get('/chat.js',function(req,res){
-res.sendFile(__dirname+'/chat.js');
+
+app.get("/chat.js", function (req, res) {
+  res.sendFile(__dirname + "/chat.js");
 });
-io.on('connection',function(socket){
-socket.on('chatmessage',function(msg){
-io.emit('chatmessage',msg);
+
+io.on("connection", function (socket) {
+  socket.on("chatmessage", function (msg) {
+    io.emit("chatmessage", msg);
+  });
 });
-});
-http.listen(3000,function(){
-console.log('listeningon*:3000');
+
+http.listen(3000, function () {
+  console.log("listeningon*:3000");
 });
